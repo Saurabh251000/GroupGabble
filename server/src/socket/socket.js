a9bba983c9c69a5f459b50bde1572834d4e784da
Saurabh Kushwaha <105183734+Saurabh251000@users.noreply.github.com>
2024-06-17 06:10:40 -0700

    deploye

--- server/src/socket/socket.js
+++ server/src/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://group-gabble-client.vercel.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
@@ -17,7 +17,7 @@ const io = new Server(server, {
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://group-gabble-client.vercel.app",
   credentials: true,
 }));
 
